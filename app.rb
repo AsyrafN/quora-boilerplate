5e1a4a72a506826e724dc47c49caf43de49bc63a
AsyrafN <asyrafnaqi@yahoo.com>
2018-09-05 16:22:04 +0800

    Add date

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require_relative './config/init.rb'
+require 'date'
 set :run, true
 
 get '/' do
   @name = "Bob Smith"
+  erb :"home"
+
+  @date = DateTime.now
+  @date.strftime "%d/%m/%Y %H:%M"
   erb :"home"
 end
