84e171b204a1e8d11732ac80a45d4db8fb386286
AsyrafN <asyrafnaqi@yahoo.com>
2018-09-06 18:23:05 +0800

    add login, logout and profile erb files

--- app.rb
+++ app.rb
@@ -3,16 +3,25 @@ require_relative './config/init.rb'
 set :run, true
 
 get '/' do
-  # @name = "Bob Smith"
-  erb :"home"
+   erb :"home"
+end
+
+get '/signup' do
+	erb :"signup"
+end
+
+get '/profile' do
+  erb :"profile"
 end
 # (email: params[:email], password: params[:password])
 post '/signup' do
  	@user = User.new
+ 	@user.first_name = params[:user][:first_name]
+ 	@user.last_name = params[:user][:last_name]
  	@user.email = params[:user][:email]
 	@user.password = params[:user][:password]
  	if @user.save
- 		erb :"signup"
+ 		redirect '/profile'
  	else
     	puts "please input valid data"
   end
@@ -20,3 +29,19 @@ post '/signup' do
 end  
 
 # params = {email: "email", password: "password"}
+
+
+post '/login' do
+  user = User.find_by_email(params[:user][:email])
+  if user.authenticate(params[:user][:password]) 
+  	session[:user_id] = user.id
+  	redirect '/profile'
+  else
+    puts "failed"
+  end
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
